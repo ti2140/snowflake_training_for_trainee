154a2b9926a7ab00f606da776c836a283d93f382
ti2140 <ti.tube140@gmail.com>
2026-05-27 19:21:41 +0900

    fic

--- dbt/mydbt/macros/load_mails_raw.sql
+++ dbt/mydbt/macros/load_mails_raw.sql
@@ -1,11 +1,11 @@
 {% macro load_mails_raw() %}
     {% set copy_sql %}
         COPY INTO {{ source('raw', 'MAILS_RAW') }}
-        FROM @{{ source('raw', 'ST_S3_MAIL') }}
+        FROM @{{ env_var('DBT_DATABASE') }}.RAW.ST_S3_MAIL
         MATCH_BY_COLUMN_NAME = CASE_INSENSITIVE
         FORCE = TRUE
     {% endset %}
 
     {% do run_query(copy_sql) %}
-    {{ log("load_mails_raw: COPY completed.", info=True) }}
+    {{ log("load_mails_raw: COPY INTO completed.", info=True) }}
 {% endmacro %}
