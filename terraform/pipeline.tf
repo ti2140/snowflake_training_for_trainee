6a12744cc748b22d845e3c2de4a4c590e7a9ad83
c-shirasaka <chihiro.shirasaka@de-life.co.jp>
2026-06-11 15:12:04 +0900

    piplife.tf Format修正

--- terraform/pipeline.tf
+++ terraform/pipeline.tf
@@ -9,7 +9,7 @@ resource "snowflake_storage_integration" "s3_int" {
   storage_aws_role_arn      = var.snowflake_aws_role_arn
   storage_allowed_locations = [
     "s3://anchor-demo-mybucket/messages/",
-    "s3://trainee02-bucket/messages/",
+    "s3://trainee02-bucket/messages/"
     ]
   comment                   = "Storage integration for S3 mail data."
 }
